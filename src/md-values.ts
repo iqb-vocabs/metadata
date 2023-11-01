c0b0f43f5c09cd8081b693cc108df2956efae61c
mechtelm <mechtel@iqb.hu-berlin.de>
2023-11-01 10:39:32 +0100

    Add md-values

---
+++ src/md-values.ts
@@ -0,0 +1,18 @@
+import {TextWithLanguage} from "./md-main";
+
+export interface TextsWithLanguageAndId {
+    id: string;
+    text: TextWithLanguage[];
+}
+
+export class MDValue {
+    readonly id: string;
+    readonly label: TextWithLanguage[];
+    value: TextsWithLanguageAndId | TextWithLanguage[] | string[] | string | null = null;
+    valueAsText: string = ''
+
+    constructor(id: string, label: TextWithLanguage[]) {
+        this.id = id;
+        this.label = label
+    }
+}
