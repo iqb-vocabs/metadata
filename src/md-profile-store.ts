aa2ba43a9d1e4c3fd6a41e373063cdc077862f99
nanoyan <yanpingchenlin@gmail.com>
2024-04-12 15:01:26 +0200

    Split creator into publisher and maintainer

--- src/md-profile-store.ts
+++ src/md-profile-store.ts
@@ -3,12 +3,14 @@ import {LanguageFactory} from "./md-main";
 export class MDProfileStore {
     readonly id: string;
     readonly title: string;
-    readonly creator: string;
+    readonly publisher: string;
+    readonly maintainer: string;
     readonly profiles: string[];
     constructor(profileStoreData: any) {
         this.id = profileStoreData['id'] || null;
         this.title = LanguageFactory.getTextFromTextWithLanguage(profileStoreData['title']);
-        this.creator = profileStoreData['creator'];
+        this.publisher = profileStoreData['publisher'] || profileStoreData['creator'];
+        this.maintainer = profileStoreData['maintainer'];
         this.profiles = [];
         if (profileStoreData['profiles']) {
             this.profiles = profileStoreData['profiles'];
